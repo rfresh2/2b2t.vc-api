4e1123d97e6b6e3ef869fb4dd4a28a2eba6ec596
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-04-05 00:36:16 -0700

    chats API

---
+++ src/main/java/vc/controller/ChatsController.java
@@ -0,0 +1,35 @@
+package vc.controller;
+
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import org.jooq.DSLContext;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import vc.data.dto.tables.pojos.Chats;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+public class ChatsController {
+
+    private final DSLContext dsl;
+
+    public ChatsController(final DSLContext dsl) {
+        this.dsl = dsl;
+    }
+
+    @GetMapping("/chats")
+    @RateLimiter(name = "main")
+    @Cacheable("chats")
+    public List<Chats> chats(@RequestParam(value = "uuid") UUID uuid, @RequestParam(value = "page", required = false) Integer page) {
+        return dsl.selectFrom(vc.data.dto.tables.Chats.CHATS)
+                .where(vc.data.dto.tables.Chats.CHATS.PLAYER_UUID.eq(uuid))
+                .orderBy(vc.data.dto.tables.Chats.CHATS.TIME.desc())
+                .limit(100)
+                .offset(page == null ? 0 : page * 100)
+                .fetch()
+                .into(vc.data.dto.tables.pojos.Chats.class);
+    }
+}
