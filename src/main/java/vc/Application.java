5a3157d738b8c20fa772aa7b5a8a0824c15c88ee
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-04-04 23:01:35 -0700

    default / mapping

--- src/main/java/vc/Application.java
+++ src/main/java/vc/Application.java
@@ -3,12 +3,20 @@ package vc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableCaching
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "hi";
 	}
 }
