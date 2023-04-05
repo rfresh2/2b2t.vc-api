37d2c63e022cf658554ea596a56621ebced87773
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-04-05 00:40:16 -0700

    deaths API's

---
+++ src/main/java/vc/controller/DeathsController.java
@@ -0,0 +1,47 @@
+package vc.controller;
+
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import org.jooq.DSLContext;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import vc.data.dto.tables.Deaths;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+public class DeathsController {
+    private final DSLContext dsl;
+
+    public DeathsController(final DSLContext dsl) {
+        this.dsl = dsl;
+    }
+
+    @GetMapping("/deaths")
+    @RateLimiter(name = "main")
+    @Cacheable("deaths")
+    public List<vc.data.dto.tables.pojos.Deaths> deaths(@RequestParam(value = "uuid") UUID uuid, @RequestParam(value = "page", required = false) Integer page) {
+        return dsl.selectFrom(vc.data.dto.tables.Deaths.DEATHS)
+                .where(Deaths.DEATHS.VICTIM_PLAYER_UUID.eq(uuid))
+                .orderBy(vc.data.dto.tables.Deaths.DEATHS.TIME.desc())
+                .limit(100)
+                .offset(page == null ? 0 : page * 100)
+                .fetch()
+                .into(vc.data.dto.tables.pojos.Deaths.class);
+    }
+
+    @GetMapping("/kills")
+    @RateLimiter(name = "main")
+    @Cacheable("kills")
+    public List<vc.data.dto.tables.pojos.Deaths> kills(@RequestParam(value = "uuid") UUID uuid, @RequestParam(value = "page", required = false) Integer page) {
+        return dsl.selectFrom(vc.data.dto.tables.Deaths.DEATHS)
+                .where(Deaths.DEATHS.KILLER_PLAYER_UUID.eq(uuid))
+                .orderBy(vc.data.dto.tables.Deaths.DEATHS.TIME.desc())
+                .limit(100)
+                .offset(page == null ? 0 : page * 100)
+                .fetch()
+                .into(vc.data.dto.tables.pojos.Deaths.class);
+    }
+}
