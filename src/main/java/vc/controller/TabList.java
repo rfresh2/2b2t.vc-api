dfa7ae7435766844019de2e40420230a3e6d9ea3
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-04-05 23:02:48 -0700

    check playercount see if server is offline before sending tablist

--- src/main/java/vc/controller/TabList.java
+++ src/main/java/vc/controller/TabList.java
@@ -9,9 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vc.data.dto.tables.pojos.OnlinePlayers;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
 
 import static vc.data.dto.tables.OnlinePlayers.ONLINE_PLAYERS;
+import static vc.data.dto.tables.Playercount.PLAYERCOUNT;
 
 @Tags({@Tag(name = "TabList")})
 @RestController
@@ -26,6 +29,14 @@ public class TabList {
     @RateLimiter(name = "main")
     @Cacheable("onlinePlayers")
     public List<OnlinePlayers> onlinePlayers() {
+        vc.data.dto.tables.pojos.Playercount playercountRecord = dsl.selectFrom(PLAYERCOUNT)
+                .orderBy(PLAYERCOUNT.TIME.desc())
+                .limit(1)
+                .fetchOne()
+                .into(vc.data.dto.tables.pojos.Playercount.class);
+        if (playercountRecord.getTime().toInstant().isBefore(Instant.now().minus(Duration.ofMinutes(10)))) {
+            return List.of();
+        }
         return dsl.selectFrom(ONLINE_PLAYERS)
                 .fetch()
                 .into(OnlinePlayers.class);
