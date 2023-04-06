693a74bf2a750bc792f133168d57c1ae5be25d86
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-04-06 13:53:07 -0700

    Redirect /error to homepage

---
+++ src/main/java/vc/controller/ErrorHandlerController.java
@@ -0,0 +1,13 @@
+package vc.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorHandlerController implements ErrorController {
+    @RequestMapping("/error")
+    public String handleError() {
+        return "redirect:/";
+    }
+}
