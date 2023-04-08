a1bac633f7dd71ec782a0d227e0fcbf7454d8342
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-04-07 18:50:51 -0700

    minor fix

--- src/main/java/vc/controller/HomepageController.java
+++ src/main/java/vc/controller/HomepageController.java
@@ -3,16 +3,15 @@ package vc.controller;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.tags.Tags;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Tags({@Tag(name = "Homepage")})
-@RestController
+@Controller
 public class HomepageController {
 
-    @GetMapping("/")
-    public void homepage(final HttpServletResponse response) {
-        response.setHeader("Location", "/swagger-ui/index.html");
-        response.setStatus(302);
+    @RequestMapping("/")
+    public String homepage(final HttpServletResponse response) {
+        return "redirect:/swagger-ui/index.html";
     }
 }
