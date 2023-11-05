771fb531730025729e9b44c87b2d9b89b354b8ff
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-11-05 01:53:58 -0700

    Player Stats API

--- src/main/java/vc/config/InitialConfiguration.java
+++ src/main/java/vc/config/InitialConfiguration.java
@@ -4,7 +4,6 @@ import org.jooq.SQLDialect;
 import org.jooq.impl.DataSourceConnectionProvider;
 import org.jooq.impl.DefaultConfiguration;
 import org.jooq.impl.DefaultDSLContext;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
@@ -13,8 +12,11 @@ import javax.sql.DataSource;
 
 @Configuration
 public class InitialConfiguration {
-    @Autowired
-    private DataSource dataSource;
+    private final DataSource dataSource;
+
+    public InitialConfiguration(final DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public DataSourceConnectionProvider connectionProvider() {
---
+++ src/main/java/vc/controller/StatsController.java
@@ -0,0 +1,59 @@
+package vc.controller;
+
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
+import org.jooq.DSLContext;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import vc.data.dto.Routines;
+import vc.util.PlayerLookup;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Tags({@Tag(name = "Stats")})
+@RestController
+public class StatsController {
+
+    private final DSLContext dsl;
+    private final PlayerLookup playerLookup;
+
+    public StatsController(final DSLContext dsl, final PlayerLookup playerLookup) {
+        this.dsl = dsl;
+        this.playerLookup = playerLookup;
+    }
+
+    @RateLimiter(name = "main")
+    @GetMapping("/stats/player")
+    public ResponseEntity<PlayerStats> playerStats(
+        @RequestParam(value = "uuid", required = false) UUID uuid,
+        @RequestParam(value = "playerName", required = false) String playerName
+    ) {
+        if (uuid == null && playerName == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        var optionalPlayerUUID = playerLookup.getOrResolveUuid(uuid, playerName);
+        if (optionalPlayerUUID.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        var resolvedUuid = optionalPlayerUUID.get();
+        var playerActivity = Routines.playerStats(dsl.configuration(), resolvedUuid)
+            .getFirst()
+            .into(PlayerStats.class);
+        return ResponseEntity.ok(playerActivity);
+    }
+
+    public record PlayerStats(
+        int joinCount,
+        int leaveCount,
+        int deathCount,
+        int killCount,
+        OffsetDateTime firstSeen,
+        OffsetDateTime lastSeen,
+        int playtimeSeconds,
+        int playtimeSecondsMonth
+    ) { }
+}
