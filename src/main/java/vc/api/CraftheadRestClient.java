5bae2a5824d0e29fe12aa1be192a6e1ebbc2cc88
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-04-07 23:25:08 -0700

    fix crafthead spring restclient not mapping json

--- src/main/java/vc/api/CraftheadRestClient.java
+++ src/main/java/vc/api/CraftheadRestClient.java
@@ -1,24 +1,36 @@
 package vc.api;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientException;
 import vc.api.model.CraftheadProfileResponse;
 import vc.api.model.ProfileData;
 
+import java.util.ArrayList;
+
 @Component
 public class CraftheadRestClient {
     private final RestClient restClient;
 
     public CraftheadRestClient(final ClientHttpRequestFactory requestFactory) {
-        this.restClient = RestClient.builder()
+        var builder = RestClient.builder()
             .baseUrl("https://crafthead.net")
             .requestFactory(requestFactory)
-            .defaultHeader("User-Agent", "2b2t.vc-api")
-            .build();
+            .defaultHeader("User-Agent", "2b2t.vc-discord");
+        builder.messageConverters(converters -> converters.stream()
+            .filter(c -> c instanceof MappingJackson2HttpMessageConverter)
+            .forEach(c -> {
+                var supportedMedia = c.getSupportedMediaTypes();
+                final ArrayList<MediaType> newMediaTypes = new ArrayList<>(supportedMedia);
+                newMediaTypes.add(new MediaType("text", "plain"));
+                ((MappingJackson2HttpMessageConverter) c).setSupportedMediaTypes(newMediaTypes);
+            })
+        );
+        this.restClient = builder.build();
     }
-
     public ProfileData getProfile(final String username) {
         var response = restClient.get()
             .uri("/profile/{username}", username)
