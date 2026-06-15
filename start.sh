0422e607a690b7d62f0857605ee67ac46bb5f995
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2026-06-14 17:25:33 -0700

    fix start.sh mvn cmd

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 set -eux
 
-mvn clean package spring-boot:repackage -DskipTests
+./mvnw clean package spring-boot:repackage -DskipTests
 java -Xmx2G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:+PerfDisableSharedMem -XX:+UseCompactObjectHeaders --sun-misc-unsafe-memory-access=allow --enable-native-access=ALL-UNNAMED -jar target/2b2t.vc-api-1.0.jar
