708aec17c5784b3f9b5178c879c8fb3cc4c49ba4
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2026-06-14 17:13:02 -0700

    update mvn wrapper

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
+set -eux
+
 mvn clean package spring-boot:repackage -DskipTests
-java -jar target/2b2t.vc-api-1.0.jar
+java -Xmx2G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:+PerfDisableSharedMem -XX:+UseCompactObjectHeaders --sun-misc-unsafe-memory-access=allow --enable-native-access=ALL-UNNAMED -jar target/2b2t.vc-api-1.0.jar
